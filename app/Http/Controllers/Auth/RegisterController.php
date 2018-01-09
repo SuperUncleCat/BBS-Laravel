c34a583f1d0cc1fd9f52a4807c23c6db4ddcc2c3
togotest2017 <togotest2017@yahoo.co.jp>
2018-01-09 12:17:34 +0900

    Add captcha

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,10 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'captcha'=>'required|captcha',
+        ],[
+          'captcha.required'=>'Captcha is not null!',
+          'captcha.captcha'=>'Wrong!',
         ]);
     }
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,22 @@
                             </div>
                         </div>
 
+                        <div class="form-group {{ $errors->has('captcha')?'has-error':'' }}">
+                          <label for="captcha" class="col-md-4 control-label">Captcha</label>
+
+                          <div class="col-md-6">
+                            <input id="captcha" class="form-control" name="captcha">
+
+                            <img class="thumbnail captcha" src="{{ captcha_src('flat') }}" onclick="this.src='/captcha/flat?'+Math.random()" title="Click to get new captcha">
+
+                            @if($errors->has('captcha'))
+                              <span class="help-block">
+                                <strong>{{ $errors->first('captcha') }}</strong>
+                              </span>
+                            @endif
+                          </div>
+                        </div>
+
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
