dec1bd808855b87bb650d02e484ac8acd0d76b87
togotest2017 <togotest2017@yahoo.co.jp>
2018-01-11 14:47:36 +0900

    List topics created by user

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -42,16 +42,23 @@
             </a>
 
             <ul class="dropdown-menu" role="menu">
-
               <li>
-                <a href="{{ route('users.edit',Auth::id()) }}">
-                  Edit
+                <a href="{{ route('users.show', Auth::id()) }}">
+                  <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                    Personal Center
+                </a>
+              </li>
+              <li>
+                <a href="{{ route('users.edit', Auth::id()) }}">
+                  <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+                    Edit Profile
                 </a>
               </li>
 
               <li>
                 <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                  Logout
+                  <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>
+                    Logout
                 </a>
 
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -38,7 +38,11 @@
     {{-- 用户发布的内容 --}}
     <div class="panel panel-default">
       <div class="panel-body">
-        暂无数据 ~_~
+        <ul class="nav nav-tabs">
+          <li class="active"><a href="#">Ta's topics</a></li>
+          <li><a href="#">Ta's replies</a></li>
+        </ul>
+        @include('users._topics',['topics'=>$user->topics()->recent()->paginate(5)])
       </div>
     </div>
 
