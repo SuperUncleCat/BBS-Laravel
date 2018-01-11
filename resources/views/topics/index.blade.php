91485a2b41d83931b27108b6654831d066ebffd8
togotest2017 <togotest2017@yahoo.co.jp>
2018-01-11 14:11:50 +0900

    Topic order

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -17,8 +17,8 @@
 
             <div class="panel-heading">
                 <ul class="nav nav-pills">
-                    <li role="presentation" class="active"><a href="#">Last Reply</a></li>
-                    <li role="presentation"><a href="#">Newest Reply</a></li>
+                    <li class="{{ active_class(( ! if_query('order', 'recent') )) }}"><a href="{{ Request::url() }}?order=default">Last Reply</a></li>
+                    <li class="{{ active_class(if_query('order', 'recent')) }}"><a href="{{ Request::url() }}?order=recent">Newest Reply</a></li>
                 </ul>
             </div>
 
