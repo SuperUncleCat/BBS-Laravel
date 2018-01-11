9b73f280273386be9b7a7dd99bd9f52f8c4ec97f
togotest2017 <togotest2017@yahoo.co.jp>
2018-01-11 15:54:27 +0900

    User can create topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -6,6 +6,8 @@ use App\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use App\Category;
+use Auth;
 
 class TopicsController extends Controller
 {
@@ -30,12 +32,18 @@ class TopicsController extends Controller
 
 	public function create(Topic $topic)
 	{
-		return view('topics.create_and_edit', compact('topic'));
+    $categories=Category::all();
+		return view('topics.create_and_edit', compact('topic','categories'));
 	}
 
-	public function store(TopicRequest $request)
+	public function store(TopicRequest $request,Topic $topic)
 	{
-		$topic = Topic::create($request->all());
+		//$topic = Topic::create($request->all());
+    $topic->fill($request->all());
+    $topic->user_id=Auth::id();
+    //$topic->excerpt = str_limit($request->body, 20, '...');
+    $topic->save();
+
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
 	}
 
--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -12,15 +12,26 @@ class TopicRequest extends Request
             case 'POST':
             {
                 return [
-                    // CREATE ROLES
+                  'title' => 'required|min:2',
+                  'body' => 'required|min:3',
+                  'category_id' => 'required|numeric',
                 ];
             }
             // UPDATE
             case 'PUT':
+            {
+                return [
+                  'title' => 'required|min:2',
+                  'body' => 'required|min:3',
+                  'category_id' => 'required|numeric',
+                ];
+            }
             case 'PATCH':
             {
                 return [
-                    // UPDATE ROLES
+                  'title' => 'required|min:2',
+                  'body' => 'required|min:3',
+                  'category_id' => 'required|numeric',
                 ];
             }
             case 'GET':
