f16fd2d2651f83b20ab1abe8e445ee30275b16bc
togotest2017 <togotest2017@yahoo.co.jp>
2018-01-16 10:03:24 +0900

    Integrated administrator

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -55,6 +55,16 @@
             </a>
 
             <ul class="dropdown-menu" role="menu">
+
+              @can('manage_contents')
+                <li>
+                  <a href="{{ url(config('administrator.uri')) }}">
+                    <span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span>
+                    Management
+                  </a>
+                </li>
+              @endcan
+
               <li>
                 <a href="{{ route('users.show', Auth::id()) }}">
                   <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
